e8a4547e19cd7721a7caf1d08d982dc0e6d4bac9
Jaime Villarreal <jvillarreal337@gmail.com>
2019-05-07 01:03:59 -0500

    Implemented artists I follow.

---
+++ GigHub/Controllers/ArtistController.cs
@@ -0,0 +1,35 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Users
+                .Where(a => a.Followers.Any(f => f.FollowerId == userId));
+
+
+            var viewModel = new ArtistsViewModel()
+            {
+                FollowedArtists = artists,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Artists I follow"
+            };
+
+            return View(viewModel);
+        }
+    }
+}
