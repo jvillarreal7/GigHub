257b8282bfc7e2f986fbe8ab8f80884d45806341
Jaime Villarreal <jvillarreal337@gmail.com>
2019-06-03 21:59:28 -0500

    Set attendance fix.

--- GigHub/Scripts/app/controllers/gigsController.js
+++ GigHub/Scripts/app/controllers/gigsController.js
@@ -1,16 +1,15 @@
-﻿//Revealing module pattern for better separation of concerns/DRY.
-
+﻿
 var GigsController = function (attendanceService) {
     var button;
 
-    var init = function () {
+    var init = function (container) {
         $(container).on("click", ".js-toggle-attendance", toggleAttendance);
     };
 
     var toggleAttendance = function (e) {
         button = $(e.target);
 
-        var gigId = button.atrr("data-gig-id");
+        var gigId = button.attr("data-gig-id");
 
         if (button.hasClass("btn-default"))
             attendanceService.createAttendance(gigId, done, fail);
@@ -21,11 +20,11 @@ var GigsController = function (attendanceService) {
     var done = function () {
         var text = (button.text() == "Going") ? "Going?" : "Going";
 
-        button.toggleClass("btn-info").toggleClass("btn.default").text(text);
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
     };
 
     var fail = function () {
-        alert("Something failed!");
+        alert("Something failed");
     };
 
     return {
